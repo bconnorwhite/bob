41010f7b71a0013d2a49b3cad552a8fe34dc6fc8
Connor White <connor.bcw@gmail.com>
2020-08-22 17:25:24 -0500

    workspace version of list and count

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,12 +1,38 @@
+import { join } from "path";
 import find from "find";
 import commander from "commander";
+import { getWorkspacePackages, exists } from "@bconnorwhite/package";
 import { getSourceDir } from "../structure";
 
-export function getFiles() {
-  return new Promise<string[]>((resolve) => {
-    find.file(/\.tsx?$/, getSourceDir().relative, (files) => {
-      resolve(files);
-    });
+export async function getFiles() {
+  return getWorkspacePackages().then(async (packages) => {
+    if(packages) {
+      let promises: Promise<string[]>[] = [];
+      Object.keys(packages).forEach((name) => {
+        const promise = new Promise<string[]>((resolve) => {
+          const packageRelative = join(packages[name].location, getSourceDir().relative);
+          exists(packageRelative).then((result) => {
+            if(result) {
+              find.file(/\.tsx?$/, packageRelative, (files) => {
+                resolve(files);
+              });
+            } else {
+              resolve([]);
+            }
+          });
+        });
+        promises.push(promise);
+      });
+      return Promise.all(promises).then((results) => {
+        return results.reduce((retval, files) => retval.concat(files), []);
+      });
+    } else {
+      return new Promise<string[]>((resolve) => {
+        find.file(/\.tsx?$/, getSourceDir().relative, (files) => {
+          resolve(files);
+        });
+      });
+    }
   });
 }
 
