80bd57c60ef9d54c28ebbc92f46159f4941b4d9b
Connor White <connor.bcw@gmail.com>
2020-09-26 13:05:36 -0500

    feat: github init

---
+++ source/commands/init/git/github.ts
@@ -0,0 +1,59 @@
+import { createCommand } from "commander-version";
+import { prompt } from "inquirer";
+import ConfigStore from "configstore";
+import { Octokit } from "@octokit/rest";
+import isGitRepo from "is-git-repository";
+import { getPackageJSON } from "../../../structure";
+import { getModuleName } from "../../../utils";
+
+export type InitGitHubArgs = {
+  configPackageName?: string;
+}
+
+export async function initGitHub(args: InitGitHubArgs = {}) {
+  if(isGitRepo()) {
+    return getPackageJSON().read().then(async (pkgJSON) => {
+      return getModuleName(args.configPackageName).then(async (moduleName) => {
+        const config = moduleName ? new ConfigStore(moduleName) : undefined;
+        return prompt([{
+          type: "input",
+          name: "name",
+          message: "GitHub repo name:",
+          default: pkgJSON?.name
+        }, {
+          type: "input",
+          name: "githubUsername",
+          message: "GitHub username:",
+          default: config?.get("githubUsername")
+        }, {
+          type: "input",
+          name: "githubToken",
+          message: "GitHub token:",
+          default: config?.get("githubToken")
+        }]).then((answers) => {
+          config?.set("githubUsername", answers.githubUsername);
+          config?.set("githubToken", answers.githubToken);
+          const octokit = new Octokit({
+            auth: answers.githubToken
+          });
+          return octokit.repos.createForAuthenticatedUser({
+            name: answers.name,
+            description: pkgJSON?.description,
+            homepage: pkgJSON?.homepage,
+            private: pkgJSON?.private
+          });
+        });
+      });
+    });
+  } else {
+    return undefined;
+  }
+}
+
+export async function initGitHubAction() {
+  initGitHub();
+}
+
+export default createCommand("github")
+  .description("initialize GitHub repo")
+  .action(initGitHubAction);
--- source/commands/init/git/index.ts
+++ source/commands/init/git/index.ts
@@ -2,22 +2,26 @@ import { createCommand } from "commander-version";
 import isGitRepo from "is-git-repository";
 import { exec } from "@bconnorwhite/exec";
 import initGitignoreCommand, { initGitignoreAction, initGitignore } from "./gitignore";
+import initGitHubCommand, { initGitHubAction, initGitHub, InitGitHubArgs } from "./github";
 
 export async function initGit() {
-  return initGitignore().then(() => {
-    if(!isGitRepo()) {
-      return exec({
-        command: "git",
-        args: [
-          "init", {
-            quiet: true
-          }
-        ]
-      });
-    } else {
-      return undefined;
-    }
-  });
+  return Promise.all([
+    initGitHub(),
+    initGitignore().then(() => {
+      if(!isGitRepo()) {
+        return exec({
+          command: "git",
+          args: [
+            "init", {
+              quiet: true
+            }
+          ]
+        });
+      } else {
+        return undefined;
+      }
+    })
+  ]);
 }
 
 export async function initGitAction() {
@@ -27,10 +31,15 @@ export async function initGitAction() {
 export default createCommand("gitignore")
   .description("initialize git repo")
   .addCommand(initGitignoreCommand)
+  .addCommand(initGitHubCommand)
   .action(initGitAction);
 
 export {
   initGitignoreCommand,
   initGitignoreAction,
-  initGitignore
+  initGitignore,
+  initGitHubCommand,
+  initGitHubAction,
+  initGitHub,
+  InitGitHubArgs
 }
