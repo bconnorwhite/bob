5c89a801331141f834957d182769baf5fa67c3aa
Connor White <connor.bcw@gmail.com>
2020-09-29 20:23:24 -0500

    fix: add support for publishing from yarn and pnpm

--- source/commands/publish.ts
+++ source/commands/publish.ts
@@ -1,10 +1,19 @@
 import { createCommand } from "commander-version";
-import { exec } from "@bconnorwhite/exec";
+import { exec, Args } from "@bconnorwhite/exec";
+import { getPackageManagerName } from "which-pm-lockfile";
 import { release } from "./release";
 
 export async function publish() {
-  return release().then(() => {
-    return exec("npm", "publish");
+  return release().then(async () => {
+    return getPackageManagerName().then((pm = "npm") => {
+      const args: Args = ["publish"];
+      if(pm === "yarn") {
+        args.push({
+          "non-interactive": true
+        });
+      }
+      return exec(pm, args);
+    });
   });
 }
 
